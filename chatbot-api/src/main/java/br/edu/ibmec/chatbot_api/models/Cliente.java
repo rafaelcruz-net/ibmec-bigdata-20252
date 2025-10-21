06b1a3e7aac347ba36a4d775040d2598ef9b80d8
Rafael Cruz <rafaelcruz.net81@gmail.com>
2025-10-21 19:36:51 -0300

    feat: Refatoração para cosmos db

--- chatbot-api/src/main/java/br/edu/ibmec/chatbot_api/models/Cliente.java
+++ chatbot-api/src/main/java/br/edu/ibmec/chatbot_api/models/Cliente.java
@@ -1,25 +1,22 @@
 package br.edu.ibmec.chatbot_api.models;
 
-import jakarta.persistence.*;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
-@Entity
-@Table(name = "clientes")
 public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String nome;
 
-    @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
     private String celular;
 
-    @Column(nullable = false, unique = true)
     private String cpf;
+
+    private List<ReservaVoo> reservasVoo;
+
+    private List<ReservaHotel> reservasHotel;
 }
--- chatbot-api/src/main/java/br/edu/ibmec/chatbot_api/models/ReservaHotel.java
+++ chatbot-api/src/main/java/br/edu/ibmec/chatbot_api/models/ReservaHotel.java
@@ -1,31 +1,18 @@
 package br.edu.ibmec.chatbot_api.models;
 
-import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-@Entity
-@Table(name = "reservas_hotel")
 public class ReservaHotel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "cliente_id", nullable = false)
-    private Cliente cliente;
-
-    @Column(nullable = false)
     private String cidade;
 
-    @Column(nullable = false)
     private String hotel;
 
-    @Column(name = "data_entrada", nullable = false)
     private LocalDate dataEntrada;
 
-    @Column(name = "data_saida", nullable = false)
     private LocalDate dataSaida;
 }
