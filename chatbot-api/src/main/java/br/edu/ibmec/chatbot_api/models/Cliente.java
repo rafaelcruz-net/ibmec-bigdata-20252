0914300b4c900585955a090d7ecc36d5f75caf27
Rafael Cruz <rafaelcruz.net81@gmail.com>
2025-10-21 20:09:17 -0300

    feat: modelagem de dados cosmos db

--- chatbot-api/src/main/java/br/edu/ibmec/chatbot_api/models/Cliente.java
+++ chatbot-api/src/main/java/br/edu/ibmec/chatbot_api/models/Cliente.java
@@ -2,19 +2,28 @@ package br.edu.ibmec.chatbot_api.models;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+
 import lombok.Data;
 
 @Data
+@Container(containerName = "clientes")
 public class Cliente {
-    private Long id;
+    
+    @Id
+    private String id;
+
+    @PartitionKey
+    private String cpf;
 
     private String nome;
 
     private String email;
 
     private String celular;
-
-    private String cpf;
 
     private List<ReservaVoo> reservasVoo;
 
