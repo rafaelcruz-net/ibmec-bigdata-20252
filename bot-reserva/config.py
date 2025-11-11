b4a29ca43e018d837a5c8e77b91e59925587e085
RAFAEL BENTO CRUZ <09072429796@professores.ibmec.edu.br>
2025-11-11 20:20:52 -0300

    feat: integração com language understand

--- bot-reserva/config.py
+++ bot-reserva/config.py
@@ -10,4 +10,8 @@ class DefaultConfig:
     PORT = 3979
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    API_BASE_URL = "http://localhost:8080/api" 
+    API_BASE_URL = "http://localhost:8080/api"
+    CLU_ENDPOINT = "https://hotelbotreservatipon.cognitiveservices.azure.com/" 
+    CLU_KEY = "2xjNBtRtzlukP2Advie631EeArdLABWBEKd1LylNbSZni0IFgHhsJQQJ99BKACHYHv6XJ3w3AAAaACOGuRki"
+    CLU_PROJECT = "HotelReservation"
+    CLU_DEPLOYMENT_NAME = "Onmi1"
